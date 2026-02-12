390ae1329178e5e5ab5c6ccfebd6bf7cf44ddb0e
rolofran <99774170+rolofran@users.noreply.github.com>
2026-02-12 16:01:38 -0300

    Remove automatic migrations and composer commands
    Removed automatic migration, composer install, and seeding steps from the entrypoint script.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -19,35 +19,6 @@ php-fpm -D
 # Esperar unos segundos para que PHP-FPM esté listo
 sleep 2
 
-# Ejecutar migraciones automáticamente
-echo "Running Laravel migrations..."
-if php artisan migrate --force; then
-    echo "✅ Migrations applied successfully."
-else
-    echo "⚠️ Migrations failed!"
-fi
-
-echo "Running composer install..."
-if composer install; then
-    echo "✅ composer install successfully."
-else
-    echo "⚠️ composer failed!"
-fi
-
-echo "Running faker install..."
-if composer require fakerphp/faker --dev; then
-    echo "✅ Faker install successfully."
-else
-    echo "⚠️ Faker install failed!"
-fi
-
-echo "Running Laravel seed..."
-if php artisan db:seed --force; then
-    echo "✅ Seeders applied successfully."
-else
-    echo "⚠️ Seeders failed!"
-fi
-
 # Arrancar Nginx en foreground
 echo "Starting Nginx..."
 nginx -g 'daemon off;'
